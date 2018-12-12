f58dd40625db1e76ab092ec5275068c438a153ab
Chuck Krutsinger <chuck.krutsinger@countermind.com>
2018-12-12 13:47:53 -0700

    significant bug fixes and refactoring

--- DrawerDemo/DrawerViewController.swift
+++ DrawerDemo/DrawerViewController.swift
@@ -8,6 +8,10 @@ private enum DrawerPosition {
     case open, closed
 }
 
+protocol DrawerView {
+    func configureDrawer(containerView: UIView, overlaidView: UIView)
+}
+
 private enum DrawerConstants {
     static let snapVelocity:CGFloat = 900
     static let animationDuration: TimeInterval = 0.5
@@ -25,23 +29,18 @@ class DrawerViewController: UIViewController {
     
     private var drawerParentView: UIView!
     private var overlaidByDrawerView: UIView!
-    private var drawerFrame: CGRect!
+    private var drawerAtTopFrame: CGRect!
+    private var drawerAtBottomFrame: CGRect!
     private var drawerTopY: CGFloat!
     private var drawerMiddleY: CGFloat!
     private var drawerBottomY: CGFloat!
     private var drawerBottomPositionOffset: CGFloat!
-    private var drawerPosition: DrawerPosition = .open //choose inital position of the sheet
+    private var drawerPosition: DrawerPosition = .closed //choose inital position of the sheet
     
     //tableview variables
     var listItems: [Any] = []
     var headerItems: [Any] = []
     
-    func configureDrawer(containerView: UIView, overlaidView: UIView) {
-        self.drawerParentView = containerView
-        self.overlaidByDrawerView = overlaidView
-        self.drawerFrame = overlaidView.frame
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -53,101 +52,136 @@ class DrawerViewController: UIViewController {
     }
     
     override func viewDidLayoutSubviews() {
-        self.drawerFrame = overlaidByDrawerView.frame
-        self.drawerTopY = drawerFrame.origin.y
-        self.drawerMiddleY = drawerTopY + drawerFrame.height * 0.5
-        self.drawerBottomY = drawerTopY + (drawerFrame.height - searchBar.frame.height)
-        self.currentDrawerY = drawerBottomY
-    }
-    
-    fileprivate func configurePanGestures() {
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
-        self.drawerView.addGestureRecognizer(panGesture)
-        self.tableView.panGestureRecognizer.addTarget(self, action: #selector(handlePan(_:)))
+        drawerAtTopFrame = overlaidByDrawerView.frame
+        view.frame = CGRect(x: 0, y: 0, width: drawerAtTopFrame.width, height: drawerAtTopFrame.height)
+        drawerTopY = drawerAtTopFrame.origin.y
+        drawerMiddleY = drawerTopY + drawerAtTopFrame.height * 0.5
+        drawerBottomY = drawerTopY + (drawerAtTopFrame.height - searchBar.frame.height)
+        drawerAtBottomFrame = CGRect(x: drawerAtTopFrame.minX, y: drawerBottomY, width: drawerAtTopFrame.width, height: drawerAtTopFrame.height)
+        
+        switch drawerPosition {
+        case .open:
+            self.currentDrawerY = drawerTopY
+        case .closed:
+            self.currentDrawerY = drawerBottomY
+        }
+        moveDrawerTo(position: drawerPosition)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        drawerParentView.frame = drawerFrame.offsetBy(dx: 0, dy: 0)
+        switch self.drawerPosition {
+        case .open:
+            drawerParentView.frame = drawerAtTopFrame
+        case .closed:
+            drawerParentView.frame = drawerAtBottomFrame
+        }
     }
     
+}
 
-    @objc func handlePan(_ recognizer: UIPanGestureRecognizer){
+extension DrawerViewController: DrawerView {
+    
+    func configureDrawer(containerView: UIView, overlaidView: UIView) {
+        self.drawerParentView = containerView
+        self.overlaidByDrawerView = overlaidView
+        self.drawerAtTopFrame = overlaidView.frame
+    }
+    
+}
+
+private extension DrawerViewController {
+    
+    func adjustDrawerFrame(_ yOffset: CGFloat) {
+        let offset: CGFloat
+        switch self.drawerPosition {
+        case .open:
+            offset = yOffset
+        case .closed:
+            offset = drawerBottomY - drawerTopY + yOffset
+        }
+        //don't offset past zero to stay within container view
+        drawerParentView.frame = drawerAtTopFrame.offsetBy(dx: 0, dy: max(0, offset))
+    }
+    
+    func animateToNextPosition(_ position: DrawerPosition) {
+        drawerPosition = position
+        drawerView.isUserInteractionEnabled = false
+        let animations = {
+            self.moveDrawerTo(position: position)
+        }
         
+        let onCompletion = { (_: Bool) in
+            self.drawerView.isUserInteractionEnabled = true
+            self.currentDrawerY = self.drawerParentView.frame.minY
+        }
+        
+        UIView.animate(withDuration: DrawerConstants.animationDuration,
+                       delay: 0,
+                       usingSpringWithDamping: DrawerConstants.dampingRatio,
+                       initialSpringVelocity: DrawerConstants.initialSpringVelocity,
+                       options: .curveEaseInOut,
+                       animations: animations,
+                       completion: onCompletion)
+    }
+    
+    func configurePanGestures() {
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handleDrawerPan(_:)))
+        self.drawerView.addGestureRecognizer(panGesture)
+        self.tableView.panGestureRecognizer.addTarget(self, action: #selector(handleTableViewPan(_:)))
+    }
+    
+    @objc func handleDrawerPan(_ recognizer: UIPanGestureRecognizer) {
         let dy = recognizer.translation(in: self.drawerParentView).y
         switch recognizer.state {
         case .changed:
-            if self.tableView.contentOffset.y > 0 { //in table
-                return
-            }
-            
-            if self.tableView.contentOffset.y <= 0 { //top of table
-                switch self.drawerPosition {
-                case .open:
-                    drawerParentView.frame = drawerFrame.offsetBy(dx: 0, dy: dy)
-                case .closed:
-                    let offset = drawerBottomY - drawerTopY + dy
-                    drawerParentView.frame = drawerFrame.offsetBy(dx: 0, dy: offset)
-                }
-            }
-            
-            if self.drawerParentView.frame.minY > drawerTopY {
-                self.tableView.contentOffset.y = 0
-            }
+            adjustDrawerFrame(dy)
         case .failed, .ended, .cancelled:
-            if (self.tableView.contentOffset.y > 0) {//inside the table
-                return
-            }
-            
-            self.currentDrawerY = self.drawerParentView.frame.minY
-            self.drawerView.isUserInteractionEnabled = false
-            self.drawerPosition = self.nextLevel(recognizer: recognizer)
-
-            UIView.animate(withDuration: DrawerConstants.animationDuration,
-                           delay: 0,
-                           usingSpringWithDamping: DrawerConstants.dampingRatio,
-                           initialSpringVelocity: DrawerConstants.initialSpringVelocity,
-                           options: .curveEaseInOut,
-                           animations: {
-                switch self.drawerPosition {
-                case .open:
-                    self.drawerParentView.frame = self.drawerFrame
-                case .closed:
-                    self.drawerParentView.frame =
-                        CGRect(x: 0, y: self.drawerBottomY, width: self.drawerFrame.width, height: self.drawerFrame.height)
-                }
-            }) { (_) in //upon completion
-                self.drawerView.isUserInteractionEnabled = true
-                self.currentDrawerY = self.drawerParentView.frame.minY
-            }
+            currentDrawerY = drawerParentView.frame.minY
+            animateToNextPosition(nextPosition(recognizer: recognizer))
         default:
             break
         }
     }
     
-    fileprivate func atTopOfTable() -> Bool {
-        return tableView.contentOffset.y == 0
+    @objc func handleTableViewPan(_ recognizer: UIPanGestureRecognizer){
+        if tableView.atTop {
+            handleDrawerPan(recognizer)
+        }
+        //else allow table to scroll
     }
     
-    fileprivate func nextLevel(recognizer: UIPanGestureRecognizer) -> DrawerPosition {
-        let velY = recognizer.velocity(in: self.view).y
-        if velY < -DrawerConstants.snapVelocity {
-            return .open
-        } else if velY > DrawerConstants.snapVelocity {
-            return atTopOfTable() ? .closed : .open
-        } else {
-            if currentDrawerY > drawerMiddleY {
-                return .closed
-            } else {
-                return .open
-            }
+    func moveDrawerTo(position: DrawerPosition) {
+        switch position {
+        case .open:
+            self.drawerParentView.frame = self.drawerAtTopFrame
+        case .closed:
+            self.drawerParentView.frame = self.drawerAtBottomFrame
         }
     }
+    
+    func nextPosition(recognizer: UIPanGestureRecognizer) -> DrawerPosition {
+        let velY = recognizer.velocity(in: self.view).y
+        let belowMidpoint = currentDrawerY > drawerMiddleY
+        
+        switch (velY, belowMidpoint) {
+        case (let vel, _) where vel < -DrawerConstants.snapVelocity:
+            return .open
+        case (let vel, _) where vel > DrawerConstants.snapVelocity:
+            return .closed
+        case (_, false):
+            return .open
+        case (_, true):
+            return .closed
+        }
+    }
+    
 }
 
 extension DrawerViewController: UITableViewDelegate, UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return 20
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -156,5 +190,13 @@ extension DrawerViewController: UITableViewDelegate, UITableViewDataSource {
         cell.configure(model: model)
         return cell
     }
+    
 }
 
+fileprivate extension UITableView {
+    
+    var atTop: Bool {
+        return self.contentOffset.y <= 0
+    }
+    
+}
--- DrawerDemo/ViewController.swift
+++ DrawerDemo/ViewController.swift
@@ -8,9 +8,20 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let vc = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "BottomSheetViewController") as! DrawerViewController
-        vc.configureDrawer(containerView: drawerContainer, overlaidView: overlaidView)
+        let vc = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "DrawerViewController") as! DrawerViewController
+        addChildViewController(vc)
+        let drawerView = vc as DrawerView
+        drawerView.configureDrawer(containerView: drawerContainer, overlaidView: overlaidView)
         drawerContainer.addSubview(vc.view)
+        drawerContainer.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            drawerContainer.topAnchor.constraint(equalTo: vc.view.topAnchor),
+            drawerContainer.bottomAnchor.constraint(equalTo: vc.view.bottomAnchor),
+            drawerContainer.leadingAnchor.constraint(equalTo: vc.view.leadingAnchor),
+            drawerContainer.trailingAnchor.constraint(equalTo: vc.view.trailingAnchor),
+
+            ])
+        vc.didMove(toParentViewController: self)
     }
     
     
